f6f854714f448f48b5886f8e370fb21feba0910f
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2011-08-24 15:23:40 +0000

    PF_RING aware drivers
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@4799 a02cd0c1-8e76-42e1-a119-56f9641475e2

---
+++ drivers/PF_RING_aware/chelsio/cxgb4/.cxgb4.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/deri/PF_RING/drivers/PF_RING_aware/chelsio/cxgb4/cxgb4.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-2.6.38-8-generic-pae/scripts/module-common.lds --build-id  -o /home/deri/PF_RING/drivers/PF_RING_aware/chelsio/cxgb4/cxgb4.ko /home/deri/PF_RING/drivers/PF_RING_aware/chelsio/cxgb4/cxgb4.o /home/deri/PF_RING/drivers/PF_RING_aware/chelsio/cxgb4/cxgb4.mod.o
