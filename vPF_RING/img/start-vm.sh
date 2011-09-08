f9a6d68c31d0429ed2d99c0c221bbd2f89f6cde4
cardigliano <cardigliano@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2011-09-08 08:23:05 +0000

    vPF_RING doc update
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@4854 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- vPF_RING/img/start-vm.sh
+++ vPF_RING/img/start-vm.sh
@@ -2,6 +2,6 @@
 -boot c \
 -hda ubuntu-amd64.img \
 -m 512 \
--netdev type=tap,id=guest0,script=if-up.sh,vhost=on -device virtio-net-pci,netdev=guest0,mac=AB:CD:EF:AB:CD:EF  \
+-netdev type=tap,id=guest0,script=if-up.sh,vhost=on -device virtio-net-pci,netdev=guest0,mac=DE:AD:BE:EF:FE:ED \
 -vnc 0.0.0.0:0 \
 -device vnplug
