15753021772dfd3364cbf3c8887823a42df29b73
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-02-02 15:52:29 +0000

    Moved to 5.6.3
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@7267 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- kernel/linux/pf_ring.h
+++ kernel/linux/pf_ring.h
@@ -41,8 +41,8 @@
 #define pfring_ptr ax25_ptr
 
 /* Versioning */
-#define RING_VERSION                "5.6.2"
-#define RING_VERSION_NUM           0x050602
+#define RING_VERSION                "5.6.3"
+#define RING_VERSION_NUM           0x050603
 
 /* Set */
 #define SO_ADD_TO_CLUSTER                 99
