fe7ba4cf0864b278247e3ceff61a1c22bfac7776
cardigliano <cardigliano@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-05-27 22:32:03 +0000

    PF_RING-aware/ZC e1000e driver update v.3.0.4.1 (testing)
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@7674 a02cd0c1-8e76-42e1-a119-56f9641475e2

---
+++ drivers/PF_RING_aware/intel/e1000e/e1000e-3.0.4.1-zc/src/load_driver.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+FAMILY=e1000e
+
+#service udev start
+
+# Remove old modules (if loaded)
+rmmod e1000e
+rmmod pf_ring
+
+echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+mkdir /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
+
+# We assume that you have compiled PF_RING
+insmod ../../../../../../kernel/pf_ring.ko
+
+insmod ./e1000e.ko
+
+sleep 1
+
+killall irqbalance 
+
+INTERFACES=$(cat /proc/net/dev|grep ':'|grep -v 'lo'|grep -v 'sit'|awk -F":" '{print $1}'|tr -d ' ')
+for IF in $INTERFACES ; do
+	TOCONFIG=$(ethtool -i $IF|grep $FAMILY|wc -l)
+        if [ "$TOCONFIG" -eq 1 ]; then
+		printf "Configuring %s\n" "$IF"
+		ifconfig $IF up
+		sleep 1
+	fi
+done
