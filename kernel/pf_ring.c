0e98c3b70aa720b59e9027839a61f9618a05f5d5
cardigliano <cardigliano@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-07-18 15:50:59 +0000

    minor fix
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@7925 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- kernel/pf_ring.c
+++ kernel/pf_ring.c
@@ -2863,7 +2863,7 @@ static inline int copy_data_to_ring(struct sk_buff *skb,
   char *ring_bucket;
   u_int64_t off;
   u_short do_lock = (
-    (enable_tx_capture && pfr->direction == rx_and_tx_direction) ||
+    (enable_tx_capture && pfr->direction != rx_only_direction) ||
     (pfr->num_channels_per_ring > 1) ||
     (pfr->channel_id_mask == RING_ANY_CHANNEL && get_num_rx_queues(skb->dev) > 1) ||
     (pfr->rehash_rss != NULL && get_num_rx_queues(skb->dev) > 1) ||
