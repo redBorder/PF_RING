222fea9041487e2bbb2e92c2dfb5131da8547616
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-10-02 07:05:33 +0000

    Added #ifdef around likely/unliley definitions
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/PF_RING@8395 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- userland/lib/pfring.h
+++ userland/lib/pfring.h
@@ -92,8 +92,13 @@
 
 /* ********************************* */
 
+#ifndef likely
 #define likely(x)       __builtin_expect((x),1)
+#endif
+
+#ifndef unlikely
 #define unlikely(x)     __builtin_expect((x),0)
+#endif
 
 /* ********************************* */
 
